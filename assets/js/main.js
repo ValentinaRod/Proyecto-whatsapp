9c213cb5944c8e389d503021ec87c7118a7af005
ValentinaRod <v.rodriguez1425@gmail.com>
2017-07-18 13:54:02 -0400

    subiendo trabajo no listo

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+
+//	que me muestre lo ingresado en el input 
+$(document).ready(function(){
+	$('input').keydown(function(){
+		$('input').html('value');
+});
+		$('input').keypress(function(){
+			$('input').html('value');
+});			
+			$('input').keyup(function(){
+				$('input').html('value')
+});
+
